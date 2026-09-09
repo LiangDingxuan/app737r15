e465ae0faeca60614cee0a6132c5b934b0ac1f72
LiangDingxuan <dingxuanliang18@gmail.com>
2026-09-09 17:03:23 +0800

    logic up to lock piece with gravity at bottom

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,53 +1,267 @@
-import {newGame, shufflePieces, newBoard, queue, hold, curPiece, gameBoard} from "./logic";
+"use client";
+import{kicks} from "./kicks";
+import{pieces} from "./pieces";
+import{board} from "./board";
+import{colours} from "./pieces";
+import {useEffect, useState, useRef} from "react";
+
+type PieceType = {
+    name: string;
+    shape: number[][];
+    colour: string;
+    }
+type BoardCell = 0 | keyof typeof colours;
+
+const defaultSpawn = {x:3, y:18}
+const createBoard = () : BoardCell[][] => board.Default.map(row => row.map(()=>0 as BoardCell))
 
 export default function Game() {
-    newGame();
+
+    const piecesList: PieceType[] = [
+        {name:"I", shape:pieces.I, colour:colours.I},
+        {name:"O", shape:pieces.O, colour:colours.O},
+        {name:"T", shape:pieces.T, colour:colours.T},
+        {name:"S", shape:pieces.S, colour:colours.S},
+        {name:"Z", shape:pieces.Z, colour:colours.Z},
+        {name:"L", shape:pieces.L, colour:colours.L},
+        {name:"J", shape:pieces.J, colour:colours.J}
+    ]
+
+    const [queue, setQueue] = useState<typeof piecesList|null>(null)
+    const [holdPiece, setHoldPiece] = useState(null);
+    const [curPiece, setCurPiece] = useState<typeof piecesList[0]|null>(null);
+    const [boardState, setBoardState] = useState<BoardCell[][]>(createBoard());
+    const [curPiecePos, setCurPiecePos] = useState(()=>(defaultSpawn))
+    const [playingState, setPlayingState] = useState(false)
+    
+
+    const stateRef = useRef({
+        holdPiece, curPiece, boardState, curPiecePos, playingState
+    })
+
+    stateRef.current = {
+        holdPiece, curPiece, boardState, curPiecePos, playingState
+    }
+
+    const handleSetGameState = (isPlaying: boolean) =>{
+        setPlayingState(isPlaying)
+    }
+
+    const handleStartGame=()=>{
+        setPlayingState(true);
+        newGame();
+    }
+
+    //const { holdPiece, curPiece, boardState, curPiecePos, playingState} = stateRef.current;
+
+    const newGame = () =>{
+        shufflePieces();
+        newBoard();
+        popQueue();
+    }
+
+    const shufflePieces = () =>{
+        const shuffledPieces = [...piecesList];
+
+        if(queue === null || queue.length === 0){
+            for(let i=0; i<piecesList.length; i++){
+                const piece = Math.floor(Math.random() * (i+1));
+                [shuffledPieces[i], shuffledPieces[piece]] = [shuffledPieces[piece], shuffledPieces[i]];
+            }
+            setQueue(shuffledPieces);
+        }
+        if(queue===null) return;
+        if(queue.length < 8){
+            const first7 = queue;
+            for(let i=0; i<piecesList.length; i++){
+                const piece = Math.floor(Math.random() * (i+1));
+                [shuffledPieces[i], shuffledPieces[piece]] = [shuffledPieces[piece], shuffledPieces[i]];
+            }
+            setQueue([...first7, ...shuffledPieces]);
+        }
+    }
+
+    const popQueue = () =>{
+        if (queue === null){
+            shufflePieces();
+            return;
+        }
+        setCurPiece(queue[0]);
+        setCurPiecePos(defaultSpawn)
+        setQueue(queue.slice(1))
+        shufflePieces();
+    }
+ 
+    const newBoard = () =>{
+        return board.Default;
+    }
+
+    const canPlace=(
+        piece: typeof curPiece, 
+        x: number, 
+        y: number, 
+        ) =>{
+        const { boardState } = stateRef.current;
+        if(!piece) return;
+        for (let dy = 0; dy < piece.shape.length; dy++){
+            for(let dx = 0; dx < piece.shape[dy].length; dx++){
+                if(piece.shape[dy][dx] === 0) continue;
+                const bx = x + dx;
+                const by = y + dy;
+
+                if (bx < 0 || bx >= boardState[0].length || by >= boardState.length) return false;
+                if (by >= 0 && boardState[by][bx] !== 0) return false;
+            }
+        }
+        return true;
+    }
+
+    const handleLockPiece=()=>{
+        const {curPiece, boardState, curPiecePos} = stateRef.current;
+        const resultLockedBoard = boardState.map(row => [...row])
+        if (!curPiece) return;
+        curPiece.shape.forEach((row,dy)=>{
+            row.forEach((cell,dx)=>{
+                if (cell === 0 ) return;
+
+                const boardY = curPiecePos.y + dy
+                const boardX = curPiecePos.x + dx
+
+                if(
+                    boardY >= 0 && 
+                    boardY < resultLockedBoard.length &&
+                    boardX >= 0 && 
+                    boardX < resultLockedBoard[0].length 
+                ){
+                    resultLockedBoard[boardY][boardX] = curPiece.name as keyof typeof colours
+                }
+            })
+        })
+        setBoardState(resultLockedBoard)
+    }
+
+    const getBoard = () =>{
+        const gameBoard = boardState.map(row => [...row])
+        if(curPiece && curPiecePos){
+            let ghostY = curPiecePos.y + 1
+            // while(canPlace(curPiece, curPiecePos.x, ghostY)){
+            //     ghostY += 1
+            // }
+            curPiece.shape.forEach((row,dy) =>{
+                row.forEach((cell, dx) =>{
+                    if(cell === 1){
+                        const boardY = dy + curPiecePos.y
+                        const boardX = dx + curPiecePos.x
+                        if(
+                            boardY >= 0 && 
+                            boardX >= 0 && 
+                            boardY < gameBoard.length && 
+                            boardX < gameBoard[0].length
+                        ) {
+                            gameBoard[boardY][boardX] = curPiece.name as keyof typeof colours
+                        }
+                        
+                    }
+
+                    // if(cell === 1){
+                    //     const boardY = dy + ghostY
+                    //     const boardX = dx + curPiecePos.x
+                    //     gameBoard[boardY][boardX] = "G"+String(curPiece.name) as keyof typeof colours
+                    // }
+                })
+            })
+        }
+        return gameBoard;
+    }
+
+    //setBoardState(getBoard())
+    const gameBoard = getBoard()
+    
+
+    useEffect(()=>{
+        newGame();
+    },[])
+
+    //gravity
+    useEffect(()=>{
+        if(!playingState) return;
+        const gameLoop = setInterval(()=>{
+            const {holdPiece, curPiece, boardState, curPiecePos} = stateRef.current;
+            const newY = curPiecePos.y + 1
+            if(canPlace(curPiece, curPiecePos.x, newY)){
+                setCurPiecePos({x:curPiecePos.x, y:newY})
+            }
+            else{
+                handleLockPiece();
+                popQueue();
+            }
+        },500);
+        return () => clearInterval(gameLoop)
+    },[playingState])
+
     return (
         <>
-        <div className="m-8 flex flex-row">
-            <div className="board">
-                <div className="topBoard ">
-                    {gameBoard.slice(16,20).map((row, rowIndex) =>(
-                        <div key={rowIndex} className="flex">
-                            {row.map((cell, cellIndex) =>(
-                                <div className="h-4 w-4">
+            <div className="m-8">
+                <div className="">
+                    <a href="/">Home</a>
+                </div>
+                <div className="testFunc gap-2 flex flex-row mt-2">
+                    <button onClick={()=>popQueue()} className="p-1 border rounded-full text-sm">Pop Queue</button>
+                    <button onClick={()=>handleStartGame()} className="p-1 border rounded-full text-sm">Start</button>
+                </div>
+                <div className=" flex flex-row">
+                    
+                    <div className="board">
+                        <div className="topBoard ">
+                            {gameBoard.slice(16,20).map((row, rowIndex) =>(
+                                <div key={rowIndex} className="flex">
+                                    {row.map((cell, cellIndex) =>(
+                                        <div key={cellIndex} className="h-4 w-4"
+                                        style={{
+                                            backgroundColor: cell === 0 ? "transparent" : colours[cell]
+                                        }}>
 
+                                        </div>
+                                    ))}
                                 </div>
                             ))}
                         </div>
-                    ))}
-                </div>
-                <div className="bottomBoard border border-white w-max">
-                    {gameBoard.slice(21,40).map((row, rowIndex) =>(
-                        <div key={rowIndex} className="flex">
-                            {row.map((cell, cellIndex)=>(
-                                <div className="border border-slate-500 h-4 w-4">
-
+                        <div className="bottomBoard border border-white w-max">
+                            {gameBoard.slice(21,40).map((row, rowIndex) =>(
+                                <div key={rowIndex} className="flex">
+                                    {row.map((cell, cellIndex)=>(
+                                        <div key={cellIndex} className="border border-slate-500 h-4 w-4" 
+                                        style={{
+                                            backgroundColor: cell === 0 ? "transparent" : colours[cell]
+                                        }}>
+                                        
+                                        </div>
+                                    ))}
                                 </div>
                             ))}
                         </div>
-                    ))}
-                </div>
-            </div>
-            <div className="queue gap-4 mt-16 pl-4 border flex flex-col">
-                {queue.slice(0,6).map(({name, shape, colour}, pieceIndex) =>(
-                    <div key={pieceIndex}className="piece flex flex-col">
-                        {shape.map((row, rowIndex)=>(
-                            <div key={rowIndex} className="flex">
-                                {row.map((cell, cellIndex)=>(
-                                    <div key={cellIndex} className="h-4 w-4" style={{
-                                    backgroundColor: cell === 1 ? colour : 'transparent',
-                                    //height: cell === 1 ? 16 : 0,
-                                    //width: cell === 1 ? 16 : 0
-                                    }}>
+                    </div>
+                    <div className="queue gap-4 mt-16 pl-4 pt-5 border flex flex-col">
+                        {queue?.slice(0,6).map(({name, shape, colour}, pieceIndex) =>(
+                            <div key={pieceIndex}className="piece flex flex-col h-8 justify-center">
+                                {shape.map((row, rowIndex)=>(
+                                    <div key={rowIndex} className="flex">
+                                        {row.map((cell, cellIndex)=>(
+                                            <div key={cellIndex} className="h-4 w-4" 
+                                            style={{
+                                            backgroundColor: cell === 1 ? colour : 'transparent',
+                                            //height: cell === 0 ? 0 : 16,
+                                            //width: cell === 0 ? 0 : 16
+                                            }}>
+                                            </div>
+                                        ))}
                                     </div>
                                 ))}
                             </div>
                         ))}
                     </div>
-                ))}
+                </div>
             </div>
-        </div>
         </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,6 @@ export default function Home() {
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-39.5"
             href="/game"
-            target="_blank"
             rel="noopener noreferrer"
           >
             Go Here
